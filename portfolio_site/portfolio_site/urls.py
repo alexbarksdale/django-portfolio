63e3d269553e10a0e12b77bc1bee7a18f70b624b
alexbarksdale <ale.barksdale@gmail.com>
2020-04-06 23:07:32 -0500

    Gradescope fix?

--- portfolio_site/portfolio_site/urls.py
+++ portfolio_site/portfolio_site/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('portfolio/', include('portfolio.urls')),
+    path('', include('portfolio.urls')),
     path('messages/', include('messageboard.urls')),
 ]
